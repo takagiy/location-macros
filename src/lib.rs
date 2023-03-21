be7bb058392bdcd41021a24e6cbaedb619507a89
takagiy <takagiy.4dev@gmail.com>
2023-03-22 01:44:36 +0900

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,36 @@ use proc_macro::TokenStream;
 mod core;
 mod error;
 
+/// Expands to a string literal that contains the absolute path of the root directory of the current crate.
+///
+/// Even if the crate is a workspace member, this expands to the path to the crate root, not the workspace root.
+/// To obtain the path to the workspace root, you can use [`workspace_dir!`].
+///
+/// # Examples
+///
+/// ```
+/// use location_macros::crate_dir;
+///
+/// let crate_dir = crate_dir!();
+/// println!("The current crate root is {}", crate_dir);
+/// ```
 #[proc_macro]
 pub fn crate_dir(input: TokenStream) -> TokenStream {
     locate(LocationKind::CrateDir, input)
 }
 
+/// Expands to a string literal that contains the absolute path of the root directory of the current workspace.
+///
+/// If the current crate is not in a workspace, this expands as same as [`crate_dir!`].
+///
+/// # Examples
+///
+/// ```
+/// use location_macros::workspace_dir;
+///
+/// let workspace_dir = workspace_dir!();
+/// println!("The current workspace root is {}", workspace_dir);
+/// ```
 #[proc_macro]
 pub fn workspace_dir(input: TokenStream) -> TokenStream {
     locate(LocationKind::WorkspaceDir, input)
